5f409d1bffaef5a8e02120a33578f5e7930028ce
Bruno Guerra <gguerrabruno@outlook.com>
2020-12-15 01:43:15 -0300

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,56 @@
+<script>
+	let notes = [];
+	let id = 0;
+	let text = "";
+
+	function handleAddNote(){
+		if(text.trim().length > 0){
+			notes.push({id, text});
+			notes = notes;
+			id = id + 1;
+			text = "";
+		}
+	}
+
+	function handleDeleteNote(note){
+		notes = notes.filter(e => e.id !== note.id);
+	}
+</script>
+
+<main>
+	<h1>Notes</h1>
+	<input type="text" name="note-text" id="note-text" bind:value={text}/>
+	<button type="button" on:click={handleAddNote}>Add note</button>
+	{#if notes.length === 0}
+		<h2>You don't have any notes 😢</h2>
+	{:else}
+		<h2>My notes</h2>
+		{#each notes as note}
+			<div class="note-container">
+				<p>{note.text} | </p><img src="trash.svg" alt="trash" on:click={handleDeleteNote(note)}/>
+			</div>
+		{/each}
+	{/if}
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
